d932cdb911fab30bcd021f57def77406311e3ee0
Nilo <nilobdrumond@gmail.com>
2020-07-17 18:36:42 -0300

    trying to remove google api key

--- src/hooks/weather.js
+++ src/hooks/weather.js
@@ -1,11 +1,11 @@
 import React, { createContext, useContext, useState, useCallback } from 'react';
 import AsyncStorage from '@react-native-community/async-storage';
+import { WEATHER_KEY } from 'react-native-dotenv';
 import {
   weatherCurrentApi,
   weatherNextApi,
   weatherApi,
 } from '../services/weatherApi';
-import { weatherkey } from '../global';
 
 const WeatherContext = createContext();
 
@@ -31,13 +31,13 @@ export const WeatherProvider = ({ children }) => {
   const getWeatherByCoordinate = useCallback(async ({ lat, lon }) => {
     try {
       let response = await weatherApi.get(
-        `?lat=-8.05&lon=-34.88&appid=${weatherkey}`,
+        `?lat=-8.05&lon=-34.88&appid=${WEATHER_KEY}`,
       );
       response = await weatherCurrentApi.get(
-        `?lat=-8.05&lon=-34.88&appid=${weatherkey}`,
+        `?lat=-8.05&lon=-34.88&appid=${WEATHER_KEY}`,
       );
       response = await weatherNextApi.get(
-        `?lat=-8.05&lon=-34.88&appid=${weatherkey}`,
+        `?lat=-8.05&lon=-34.88&appid=${WEATHER_KEY}`,
       );
       console.log(response);
     } catch (err) {
